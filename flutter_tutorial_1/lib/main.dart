aca58d3ceb68648922e4441ca2d743b3eb62befd
hilal-coskun <hilal.coskun20@gmail.com>
2022-02-08 16:09:32 +0300

    add main page

--- flutter_tutorial_1/lib/main.dart
+++ flutter_tutorial_1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorial_1/views/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,105 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //not visible debug text
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        //visible topbar
-        appBar: AppBar(
-          title: const Text('Flutter - Tutorial'),
-          centerTitle: true,
-          backgroundColor: Colors.pink.shade400,
-        ), 
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print('Flutter');
-          },
-          child: const Icon(
-            Icons.add
-          ),
-        ),
-        body: Column(
-          //Position box
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          //%100 width
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            //Row and Container types
-            /*
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: const [
-                Icon(
-                  Icons.accessible, 
-                  color: Colors.red,
-                  size: 50,
-                ),
-                Icon(
-                  Icons.access_alarm,
-                  color: Colors.blue,
-                  size: 50,
-                ),
-                Icon(
-                  Icons.account_circle_outlined,
-                  color: Colors.yellow,
-                  size: 50,
-                ),
-                Icon(
-                  Icons.add_call,
-                  color: Colors.amber,
-                  size: 50,
-                ),
-              ],
-            ),
-            Container(
-              color: Colors.amber,
-              height: 100,
-              width: 100,
-              child: const Center(
-                child: Padding(
-                  padding: EdgeInsets.all(30),
-                  child: Text('Flutter'),
-                )
-              ),
-            ),
-            Container(
-              color: Colors.blue,
-              height: 100,
-              width: 100,
-              child: const Center(
-                child: Text('Flutter')
-                ),
-            ),
-            Container(
-              color: Colors.red,
-              height: 100,
-              width: 100,
-              child: const Center(
-                child: Text('Flutter')
-                ),
-            )*/
-
-            //button types
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Login')
-            ),
-            const Text(
-              'Hilal',
-              style: TextStyle(
-                color: Colors.amber, 
-                fontSize: 30, 
-                fontWeight: FontWeight.bold 
-              ),
-            ),
-
-            //IMAGE
-            //Your upload image and path
-            //Image.asset('assets/images/flutterImage.jpg'),
-            //Website url
-            //Image.network('url')
-          ],
-        )),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
     );
   }
 }
---
+++ flutter_tutorial_1/lib/views/home_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Gezi Rehberim', 
+          style: TextStyle(color: Colors.black54)
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          right: 20.0, left: 20.0, top: 50, bottom: 100,
+        ),
+        child: Column(
+          //Butonlar sayfaya hizalandı
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          //Sayfa sonuna taşındı (Column içindeki tüm widgetları)
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+              ),
+              onPressed: () {
+              print('Log in button');
+            },
+            child: const Text('Login', style: TextStyle(color: Colors.blue, fontSize: 18, fontWeight: FontWeight.bold),)),
+
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+              ),
+              onPressed: () {
+              print('Sign Up button');
+            }, 
+            child: const Text('Sign Up', style: TextStyle(color: Colors.blue, fontSize: 18, fontWeight: FontWeight.bold),)
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
