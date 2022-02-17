15e7b37935237afa2a95aca92d9d4b73393a8a1f
hilal-coskun <hilal.coskun20@gmail.com>
2022-02-17 21:23:08 +0300

    created responsive-page-template

---
+++ instagram_clone/lib/responsive/mobile_screen_layout.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MobilScreenLayout extends StatelessWidget {
+  const MobilScreenLayout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('This is Mobile')
+      ),
+    );
+  }
+}
